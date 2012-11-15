a78f3a26a4e383e2f7e27f93e141c885f8cc94ae
Bernhard Rode <mail@bernhardrode.de>
2012-11-15 20:26:45 +0100

    Fixed LINK view

--- src/php/content-link-subpages.php
+++ src/php/content-link-subpages.php
@@ -13,23 +13,46 @@ get_header(); ?>
 <div class="container site-content">
   <div class="row">
     <div class="span12">
-      <h1><?php echo the_title(); ?></h1>
       <?php
         $child_pages = $wpdb->get_results("SELECT * FROM $wpdb->posts WHERE post_parent = ".$post->ID." AND post_status = 'publish' AND post_type = 'page' ORDER BY menu_order", 'OBJECT');    
       ?>
       <?php if ( $child_pages ) : foreach ( $child_pages as $child_page ) : setup_postdata( $child_page ); ?>
         <section id="section-<?php echo $child_page->ID; ?>">
           <a name="#section-<?php echo $child_page->ID; ?>"></a>
-          <h2><?php echo get_the_title($child_page->ID); ?></h2>
-          <?php echo $child_page->post_content; ?>
-          <?php
-            $child_child_pages = $wpdb->get_results("SELECT * FROM $wpdb->posts WHERE post_parent = ".$child_page->ID." AND post_status = 'publish' AND post_type = 'page' ORDER BY menu_order", 'OBJECT');    
-          ?>
-          <?php if ( $child_child_pages ) : foreach ( $child_child_pages as $child_child_page ) : setup_postdata( $child_child_page ); ?>
-            <h3><?php echo $child_child_page->post_title; ?></h3>
-            <?php $url = sgb_thumbnail('circle-thumb',$child_child_page->ID); ?>
-            <img src="<?php echo sgb_thumbnail('circle-thumb',$child_child_page->ID); ?>" class="img-circle img-shadow">
-          <?php endforeach; endif; ?>  
+          <div class="row">
+            <div class="span3">
+              <h2><?php echo get_the_title($child_page->ID); ?></h2>
+            </div>
+            <div class="span9">
+              <?php
+                $child_child_pages = $wpdb->get_results("SELECT * FROM $wpdb->posts WHERE post_parent = ".$child_page->ID." AND post_status = 'publish' AND post_type = 'page' ORDER BY menu_order", 'OBJECT');    
+              ?>
+              <div class="row">
+              <?php $counter = 0; ?>
+              <?php if ( $child_child_pages ) : foreach ( $child_child_pages as $child_child_page ) : setup_postdata( $child_child_page ); ?>
+                <?php $counter++; ?>
+                <div class="span2">
+                  <?php $url = sgb_thumbnail('circle-thumb',$child_child_page->ID); ?>
+                  <a class="" href="#">
+                    <img class="media-object img-max-height-200" src="<?php echo $url; ?>">
+                  </a>
+                </div>
+                <div class="span2">
+                  <div class="media-body">
+                    <h4 class="media-heading"><?php echo $child_child_page->post_title; ?></h4>
+                    <p>Lorem ipsum</p>
+                  </div>
+                </div>
+                <?php 
+                  if($counter == 2) {
+                    echo '</div><div class="row">';
+                    $counter = 0;
+                  }
+                ?>
+              <?php endforeach; endif; ?>
+              </div>
+            </div>  
+          </div>
         </section>
       <?php endforeach; endif; ?>  
     </div>
