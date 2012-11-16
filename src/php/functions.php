bc32b35b6bc2131aac64dd5ec0c6adc594b5659e
bernhardrode <mail@bernhardrode.de>
2012-11-16 08:27:16 +0100

    Added SVG Fallback for older Browsers

--- src/php/functions.php
+++ src/php/functions.php
@@ -55,6 +55,7 @@ function sgb_scripts_styles() {
   //wp_enqueue_script('bootstrap-affix', get_template_directory_uri().'/lib/bootstrap-image-gallery.js', array('jquery'), '2.8.1', true);
   //wp_enqueue_script('bootstrap-calendar', get_template_directory_uri().'/lib/bootstrap.calendar.js', array('jquery'), '2.2.1', true);
   wp_enqueue_script('bootstrap-calendar', get_template_directory_uri().'/lib/fullcalendar.js', array('jquery'), '1.5.4', true);
+  wp_enqueue_script('svgeezy', get_template_directory_uri().'/lib/svgeezy.js', array('jquery'), '1.0', true);
   //wp_enqueue_script('app', get_template_directory_uri().'/lib/socialite.js', array('jquery'), '1.0', true);
   wp_enqueue_script('app', get_template_directory_uri().'/js/app.js', array('jquery'), '1.0', true);
   //wp_enqueue_script('google-plus', 'https://apis.google.com/js/plusone.js', array(), '1.0', true);
