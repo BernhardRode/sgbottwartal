6ac17c38ee845ccb6c6d0df4b385a11f2ccbef61
Bernhard Rode <mail@bernhardrode.de>
2012-11-16 18:42:03 +0100

    Moved Kommentare

--- src/php/single.php
+++ src/php/single.php
@@ -14,11 +14,17 @@ get_header(); ?>
 				<div class="span12">
 					<?php while ( have_posts() ) : the_post(); ?>
 						<?php get_template_part( 'content', get_post_type() ); ?>
+				</div>
+			</div>
+			<div class="row">
+				<div class="offset3 span9">
 						<?php
 							// If comments are open or we have at least one comment, load up the comment template
 							if ( comments_open() || '0' != get_comments_number() )
 								comments_template( '', true );
 						?>
+				</div>
+			</div>
 					<?php endwhile; // end of the loop. ?>
 				</div>
 		</div><!-- #content -->
