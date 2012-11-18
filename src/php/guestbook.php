cb3d3d9094c82abee5fb8d1706107806ae315e85
Bernhard Rode <mail@bernhardrode.de>
2012-11-18 10:36:00 +0100

    Fixed

--- src/php/guestbook.php
+++ src/php/guestbook.php
@@ -19,6 +19,12 @@ get_header(); ?>
 	<div id="content" role="main">
 		<div class="row">
 			<?php while ( have_posts() ) : the_post(); ?>
+				<div class="span9">
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template
+						if ( comments_open() || '0' != get_comments_number() ) comments_template( '', true );
+					?>
+				</div>
 				<div class="span3">
 	        <div class="hidden-phone">
       			<?php echo do_shortcode( '[sponsoren id="5783" span="3"]' ); ?>
@@ -32,28 +38,15 @@ get_header(); ?>
 	          <?php do_shortcode( '[sponsoren count="3" span="1"]' ); ?>
 	        </div>
 				</div>
-				<div class="span9">
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template
-						if ( comments_open() || '0' != get_comments_number() ) comments_template( '', true );
-					?>
-				</div>
 			<?php endwhile; // end of the loop. ?>
 		</div>
 	  <div class="row hidden-phone">
 	    <div class="span12">
 	      <?php do_shortcode( '[sponsoren count="6" span="2"]' ); ?>
 	    </div>
-	  </div>  
-		<div class="row">
-			<div class="span12">
-      			<?php echo do_shortcode( '[fotos]' ); ?>
-			</div>
-		</div>
+	  </div>
 	</div><!-- #content -->
 </div><!-- #primary -->
-
-
 <div id="primary" class="site-content container">
 	<div id="content" role="main">
 		<div class="row">
