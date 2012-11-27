37018cf953ab009a069751d016811724e0376f67
Bernhard Rode <mail@bernhardrode.de>
2012-11-27 19:36:57 +0100

    status… not post_status

--- src/php/functions.php
+++ src/php/functions.php
@@ -435,7 +435,7 @@ function sgb_sponsoren( $args ) {
 
 function sgb_kommentare( $args ) {
   if(empty($args['count'])) $args['count'] = 5;
-  $query = array( 'number' => $args['count'], 'post_status' => 'publish' );
+  $query = array( 'number' => $args['count'], 'status' => 'approve' );
   $comments = get_comments( $query );
   echo '<ul class="unstyled">';
   foreach($comments as $comment) :
