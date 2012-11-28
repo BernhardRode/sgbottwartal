65d84a1edfac3ff04d34cb853873f46681852a3c
bernhardrode <mail@bernhardrode.de>
2012-11-28 08:22:45 +0100

    added background image...

--- src/php/header.php
+++ src/php/header.php
@@ -35,6 +35,7 @@
 ================================================== -->
 <!-- Wrap the .navbar in .container to center it on the page and provide easy way to target it with .navbar-wrapper. -->
 <header>
+  <div class="site-bg"></div>
   <div class="container navbar-wrapper">
     <div class="navbar">
       <div class="navbar-inner">
