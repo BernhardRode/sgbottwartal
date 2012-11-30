b935aaeb96abff44847df3b60b109f7bc1ab9b6a
Bernhard Rode <mail@bernhardrode.de>
2012-11-30 18:32:32 +0100

    Disk Cache for Events

--- src/plugins/json-api/controllers/sgb.php
+++ src/plugins/json-api/controllers/sgb.php
@@ -6,82 +6,21 @@ Controller description: SGB introspection methods
 
 class JSON_API_SGB_Controller {
 
-  public function get_events() {
-    $start = $_GET["start"];
-    $end = $_GET["end"];
-
-    if (!$start) $start=0;
-    if (!$end) $end=99999999999;
-
-    $args = array( 'post_type' => 'event' );
-    $loop = new WP_Query( $args );
-
-    $events = array();
-    $tmp = array();
-    while ( $loop->have_posts() ) : $loop->the_post();
-      $allday = false;
-      $meta  = get_post_meta(get_the_ID(),'meta');
-      $evt_start = '';
-      $evt_end = '';
-
-      if ($meta[0]['begin']) $evt_start = new DateTime($meta[0]['begin']);
-      if ($meta[0]['end']) $evt_end = new DateTime($meta[0]['end']);
-
-
-      if (!$evt_end) {
-        $evt_end = $evt_start;
-        $allday = true;
-      }
-      //if ($evt_end) $evt_end = $evt_start;
-
-      if ($allday) {
-        $time_start = date('Y-m-d',$evt_start->getTimestamp());
-      } else {
-        $time_start = date('Y-m-d H:m',$evt_start->getTimestamp());
-        $time_end = date('Y-m-d H:m',$evt_end->getTimestamp());
-      }
-
-      $tmp = array();
-      $tmp['id'] = intval( get_the_ID() );
-      $tmp['title'] = get_the_title();
-      $tmp['start'] = $time_start;
-      if ($allday) {
-        $tmp['allDay'] = $allday;
-      } else {
-        $tmp['end'] = $time_end;
-      }
-
-      if ( $evt_start->getTimestamp() >= $start && $evt_start->getTimestamp() <= $end ) {
-        array_push($events, $tmp); 
-      }
-    endwhile;
-
-    $string = file_get_contents("wp-content/plugins/json-api/controllers/spielplan.json");
-    $json   = json_decode($string,true);
-
-    foreach ( $json as $game) :
-
-      $game_start = new DateTime( $game['datetime']['date']);
-
-      if ( $game_start->getTimestamp() >= $start && $game_start->getTimestamp() <= $end ) {
-        $tmp = array();
-        $year = date('Y');
-        $month = date('m');
-        
-        $tmp['id'] = intval( $game['Nummer'] );
-        $tmp['title'] = strtoupper($game['tags'][0]) . ' - ' . $game['tags'][1];
-        $tmp['start'] = $game_start->getTimestamp()-3600;
-        $tmp['end'] = $game_start->getTimestamp();
-        $tmp['allDay'] = false;
-        $tmp['originalEvent'] = $game;
-
-
-        array_push($events, $tmp); 
-      }
-    endforeach;
-
-    return array($events);
+  public function save_cache($data, $key) {
+    $key = md5($key);
+    $data = serialize($data);
+    file_put_contents(getcwd().'/wp-content/cache/'.$key, $data);        
   }
+
+  public function load_cache($key, $expire) {
+    $key = md5($key);
+    $path = getcwd().'/wp-content/cache/'.$key;
+    if(time() < (filemtime($path) + $expire)) {
+        return unserialize(file_get_contents($path));
+    }
+    unlink($path);
+    return false;
+  }  
 
   public function get_events_from_wp() {
     global $json_api;
@@ -124,12 +63,59 @@ class JSON_API_SGB_Controller {
     }
 
     return array(
-      'results' => count( $games ),
+      'results' => count( $events ),
       'events' => $events
     );
   }
 
   public function update_events() {
+    global $json_api;
+
+    $ttl = 3600*24;
+    $key = 'hvw_data';
+    $hvw = $this->load_cache( $key, $ttl );
+    if ( !$hvw ) {
+      $hvw = $this->get_events_from_hvw();
+      $this->save_cache( $events, $key );
+    }
+    $ttl = 3600;
+    $key = 'wp_data';
+    $sgb = $this->load_cache( $key, $ttl );
+    if ( !$sgb ) {
+      $sgb = $this->get_events_from_wp();
+      $this->save_cache( $events, $key );
+    }
+
+    $events = array_merge($sgb['events'], $hvw['events']);
+
+    return $events;
+  }
+
+  public function get_events() {
+    $start = $_GET["start"];
+    $end = $_GET["end"];
+
+    if (!$start) $start=0;
+    if (!$end) $end=9999999999;
+
+    $key = 'from_'.$start.'_till_'.$end;
+
+    $ttl = 3600*1;
+    $events = $this->load_cache($key, $ttl);
+    $cached = true;
+    if ( !$events ) {
+      $all_events = $this->update_events();
+      $events = array();
+      foreach ($all_events as $value) {
+        if ( $value->start >= $start && $value->end <= $end ) {
+          array_push( $events, $value );
+        }
+      }
+      $this->save_cache($events, $key);
+      $cached = false;
+    }
+
+    return array( 'cached' => $cached, 'start' => $start , 'end' => $end, 'events' => $events );
   }
 }
 
