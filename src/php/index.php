62bf8a62e528f7a4a8ec262cae8a134edf97bc63
Bernhard Rode <mail@bernhardrode.de>
2013-05-21 21:35:38 +0200

    Added new slidewr

--- src/php/index.php
+++ src/php/index.php
@@ -15,38 +15,32 @@ get_header(); ?>
 <div class="site-content container">
   <div class="row hidden-phone">
     <div class="span12">
-     <div id="myCarousel" class="carousel slide">
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class=""></li>
-          <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="2" class=""></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="item">
-            <img src="assets/img/bootstrap-mdo-sfmoma-01.jpg" alt="">
-            <div class="carousel-caption">
-              <h4>First Thumbnail label</h4>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            </div>
-          </div>
-          <div class="item active">
-            <img src="assets/img/bootstrap-mdo-sfmoma-02.jpg" alt="">
-            <div class="carousel-caption">
-              <h4>Second Thumbnail label</h4>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            </div>
-          </div>
-          <div class="item">
-            <img src="assets/img/bootstrap-mdo-sfmoma-03.jpg" alt="">
-            <div class="carousel-caption">
-              <h4>Third Thumbnail label</h4>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            </div>
-          </div>
-        </div>
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
-      </div>
+      <!-- Carousel
+      ================================================== -->
+      <div id="jms-slideshow" class="jms-slideshow">
+          <?php if ( have_posts() ) : ?>
+          <?php /* Start the Loop */ ?>
+          <?php $count = 0; ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+              <?php $count = $count+1; ?>
+              <?php $random = rand(0, 360); ?>
+              <?php
+                $url = sgb_thumbnail('medium',$post->ID);
+              ?>
+              <div class="step" data-y="<?php echo $count; ?>00" data-scale="0.<?php echo $count*$random; ?>" data-rotate-x="<?php echo $random; ?>">
+                <div class="jms-content">
+                  <img src="<?php echo $url; ?>" alt="<?php echo the_title(); ?>">
+                  <h1>
+                    <?php echo the_title(); ?><?php comments_number('', '<span class="badge pull-right">1 Kommentar</span>', '<span class="badge pull-right">% Kommentare</span>' );?>
+                  </h1>
+                  <?php echo the_excerpt(); ?>
+
+                  <a href="<?php echo get_permalink(); ?>" class="jms-link btn btn-danger btn-large pull-right"><strong>Weiterlesen...</strong></a>
+                </div>
+              </div>
+            <?php endwhile; ?>
+          <?php endif; ?>
+      </div><!-- /.da-slider -->
     </div>
   </div>
   <div class="row hidden-phone">
