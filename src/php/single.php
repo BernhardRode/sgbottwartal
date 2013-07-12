8f1bbd5b59691b910c0e7813ffb625efe7b58bb6
Bernhard Rode <mail@bernhardrode.de>
2013-07-12 19:19:51 +0200

    more buttons arrr

--- src/php/single.php
+++ src/php/single.php
@@ -12,6 +12,12 @@ get_header(); ?>
 		<div id="content" role="main">
 			<div class="row">
 				<div class="span12">
+							
+		            <p> 
+		              <div class="g-plusone" data-size="medium" data-annotation="none" data-href="<?php the_permalink(); ?>"></div>
+		              <div class="fb-like" data-href="<?php the_permalink(); ?>" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div>
+		              <a href="https://twitter.com/share" class="twitter-share-button" data-lang="de" data-url="<?php the_permalink(); ?>" data-count="none" data-related="sgbottwartal" data-hash="sgb">Tweet</a>
+		            </p>
 					<?php while ( have_posts() ) : the_post(); ?>
 						<?php get_template_part( 'content', get_post_type() ); ?>
 	      		<?php //echo do_shortcode( '[gallery link="file" order="DESC" columns="10" orderby="title"]' ); ?>
