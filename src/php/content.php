08d8530a635c38b85f432479478afbab1c53d1e3
Bernhard Rode <mail@bernhardrode.de>
2013-07-12 17:45:27 +0200

    move to scyscraper

--- src/php/content.php
+++ src/php/content.php
@@ -29,10 +29,10 @@
 
       <script type="text/javascript"><!--
       google_ad_client = "ca-pub-3681567567860543";
-      /* SG Bottwartal */
-      google_ad_slot = "5752173898";
-      google_ad_width = 300;
-      google_ad_height = 250;
+      /* SG Bottwartal Skyscraper */
+      google_ad_slot = "8234635492";
+      google_ad_width = 120;
+      google_ad_height = 600;
       //-->
       </script>
       <script type="text/javascript"
