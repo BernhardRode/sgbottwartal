e7a3831917ce0fd981b5e536f4258d22a2d3a978
Bernhard Rode <mail@bernhardrode.de>
2013-07-12 17:16:46 +0200

    Searchform

---
+++ src/php/searchform.php
@@ -0,0 +1,6 @@
+<form class="form-search" role="search" method="get" id="searchform" action="/">
+  <div class="input-prepend">
+    <button type="submit" class="btn btn-danger">Suche</button>
+    <input type="text" class="span2 search-query" value="" name="s" id="s">
+  </div>
+</form>
